a10395e363ac742acf119309f2e69cc8c352907c
unknown <phamvietcuong12@gmail.com>
2021-12-13 04:44:00 +0700

    spring boot crud 1 bang

---
+++ src/main/java/com/codegym/demo/controller/CustomerController.java
@@ -0,0 +1,79 @@
+package com.codegym.demo.controller;
+
+import com.codegym.demo.model.Customer;
+import com.codegym.demo.service.ICustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+public class CustomerController {
+    ICustomerService customerService;
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer create successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomers() {
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customerService.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/customer/edit");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        } else {
+            return new ModelAndView("/404");
+        }
+    }
+
+
+    @PostMapping("/edit-customer")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+
+        } else {
+            return new ModelAndView("/error.404");
+        }
+    }
+
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.remove(customer.getId());
+        return "redirect:customers";
+    }
+}
---
+++ src/main/java/com/codegym/demo/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.codegym.demo.service;
+
+import com.codegym.demo.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer>{
+
+}
